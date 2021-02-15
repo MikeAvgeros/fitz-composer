51cef4279f020069b48137d50bc84de70badedda
Mike Avgeros <mike.avgeros@gmail.com>
2021-02-15 18:01:15 +0000

    Used javascript to connect my contact and subscribe form with the emailjs sdk.

---
+++ assets/javascript/script.js
@@ -0,0 +1,33 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "fitz", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.description.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
+function subscribe(subscribeForm) {
+    emailjs.send("gmail", "fitz", {
+        "from_name": "Website Visitor",
+        "from_email": subscribeForm.email.value,
+        "message": "Please add me to your mailing list"
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
