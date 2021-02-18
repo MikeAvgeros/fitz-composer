592be1fd89bb7f9575d751352579a0bd1b6fbe81
Mike Avgeros <mike.avgeros@gmail.com>
2021-02-18 22:51:45 +0000

    Updated the javascript function for the viewport height

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,9 @@
 let vh = window.innerHeight * 0.01;
-
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 function sendMail(contactForm) {
     emailjs.send("gmail", "fitz", {
