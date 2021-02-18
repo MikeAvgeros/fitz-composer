dc71e28e9d04a4b9427945f428cfcfdd5c5dd5f3
Mike Avgeros <mike.avgeros@gmail.com>
2021-02-18 22:23:12 +0000

    removed eventlistener from javascript as it created a bug on mobiles.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,6 @@
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 function sendMail(contactForm) {
     emailjs.send("gmail", "fitz", {
