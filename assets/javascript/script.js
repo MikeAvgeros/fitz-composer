2352c3cd42612d3bcde402cb6b4194bad938ec00
Mike Avgeros <mike.avgeros@gmail.com>
2021-02-18 22:14:37 +0000

    Bug fix: landing page height on mobiles overflows. Increased the font size of the quotes.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,8 @@
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "fitz", {
         "from_name": contactForm.name.value,
