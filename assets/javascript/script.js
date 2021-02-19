18074dacecafcd4ed7e1569dcb5af0f64a8e8f51
Mike Avgeros <mike.avgeros@gmail.com>
2021-02-19 10:16:52 +0000

    Worked on the viewport height bug on mobiles.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,8 @@
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+window.onload = function {
+    let vh = window.innerHeight;
+    document.getElementById("landing").style.height = vh;
+    document.getElementById("home").style.height = vh;
+};
 
 function sendMail(contactForm) {
     emailjs.send("gmail", "fitz", {
